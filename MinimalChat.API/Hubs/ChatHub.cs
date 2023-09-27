f984f3a9051de801aa305b3104a89b5524f2a70c
Vipul143212 <vipulu563@gmail.com>
2023-09-27 11:59:47 +0530

    Add real time chat with SignalR

---
+++ MinimalChat.API/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace MinimalChat.API.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            try
+            {
+                // Your message handling logic
+                await Clients.All.SendAsync("ReceiveMessage", user, message);
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions and log errors
+                Console.WriteLine($"Error in SendMessage: {ex.Message}");
+            }
+        }
+    }
+}
